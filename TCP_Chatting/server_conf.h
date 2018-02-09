130558d87751ac23f56d54e0aa3edf3b68710641
llHoYall <hoya128@gmail.com>
2018-02-09 14:02:36 +0900

    Add TCP_Chatting program

---
+++ TCP_Chatting/server_conf.h
@@ -0,0 +1,17 @@
+/*******************************************************************************
+ * @file		server_conf.h
+ * @brief		Configure a multiplexing TCP Server.
+ * @author	llHoYall <hoya128@gmail.com>
+ *******************************************************************************
+ * @version	v1.0
+ * @note
+ *	- 2018.02.09	Created.
+ ******************************************************************************/
+
+#ifndef __HoYa_SERVER_CONFIGURATION_H__
+#define __HoYa_SERVER_CONFIGURATION_H__
+
+/* Definitions  --------------------------------------------------------------*/
+#define MAX_CLIENT		5
+
+#endif
